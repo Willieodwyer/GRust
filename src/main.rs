bf6ba29392708bc988b0afdb092fa06ce2fb449a
William O'Dwyer <william.odwyer@digitalbarriers.com>
2024-03-29 13:01:19 +0000

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use eframe::egui;
 
 fn main() {
+    println!("Hello!");
+    let frames = 100;
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native("My egui App", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
+
+    eframe::run_native(
+        "My egui App",
+        native_options,
+        Box::new(|cc| Box::new(MyEguiApp::new(cc))),
+    );
 }
 
 #[derive(Default)]
@@ -19,9 +26,18 @@ impl MyEguiApp {
 }
 
 impl eframe::App for MyEguiApp {
-   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-       egui::CentralPanel::default().show(ctx, |ui| {
-           ui.heading("Hello World!");
-       });
-   }
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        static mut FRAMES: i64 = 0;
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let mut message = "Hello World! Frame number: ".to_owned();
+            let mut frames: String;
+            unsafe {
+                FRAMES += 1;
+                frames = FRAMES.to_string();
+            }
+            
+            message.push_str(&frames);
+            ui.heading(message);
+        });
+    }
 }
